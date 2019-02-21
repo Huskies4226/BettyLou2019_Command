5c842dfe4f20ee1fc944cb5e517aee11f513a576
unknown <HuskiesRobotics4226@gmail.com>
2019-02-20 19:19:32 -0600

    End of day Elevator

---
+++ src/main/java/org/usfirst/frc4226/BettyLou2019_Command/commands/Elevator_Jog.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc4226.BettyLou2019_Command.commands;
+
+import org.usfirst.frc4226.BettyLou2019_Command.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Elevator_Jog extends Command {
+  public Elevator_Jog() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.elevator);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double speed = Robot.oi.coDiverStick.getRawAxis(5) * -1;
+    Robot.elevator.my_Elevator_Jog(speed);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ src/main/java/org/usfirst/frc4226/BettyLou2019_Command/util/CouldCrashException.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc4226.BettyLou2019_Command.util;
+
+public class CouldCrashException extends Exception {
+	private static final long serialVersionUID = 12345678L;  // unique id
+}
